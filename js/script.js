3b2f42078a24eef05de4cb43e0b1e5723e44b9b1
jon1408 <40411510+jon1408@users.noreply.github.com>
2023-05-08 23:00:33 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,46 @@
-const calcular =document.getElementById('calcular');
+const calcular = document.getElementById('calcular');
 
-function imc (){
-    const nome =document.getElementById('nome').value;
-    const altura =document.getElementById('altura').value;
-    const peso =document.getElementById('peso').value;
-    const resultado =document.getElementById('resultado');
+// Função para calcular o IMC
+function calcularIMC() {
+  const nome = document.getElementById('nome').value;
+  const altura = parseFloat(document.getElementById('altura').value);
+  const peso = parseFloat(document.getElementById('peso').value);
+  const resultado = document.getElementById('resultado');
 
-    if (nome !== '' && altura !== '' && peso !== ''){
-        
-        const valorIMC = (peso / (altura * altura)).toFixed(1);
+  // Verificar se os campos estão vazios ou não contêm valores válidos
+  if (!nome || isNaN(altura) || isNaN(peso)) {
+    resultado.textContent = 'Por favor, preencha todos os campos com valores válidos.';
+    return;
+  }
 
-        let classificacao = '';
+  // Calcular o valor do IMC
+  const valorIMC = (peso / (altura * altura)).toFixed(1);
 
-        if (valorIMC <= 18.5){
-            classificacao = 'abaixo do peso';
-        }else if( valorIMC >= 18.6 && valorIMC <= 24.9){
-            classificacao = 'no peso ideal. Parábens!!!';
-        }else if (valorIMC >= 25 && valorIMC <= 29.9){
-            classificacao = 'levemente acima do peso';
-        }else if (valorIMC >= 30 && valorIMC <=34.9 ){
-            classificacao = 'com obesidade grau I, é melhor procurar ajuda médica.';
-        }else if (valorIMC  >= 35 && valorIMC <= 39.9){
-            classificacao = 'com obesidade grau II (severa), é melhor procurar ajuda médica.';
-        }else if (valorIMC > 40){
-            classificacao = 'com obesidade grau III (mórbida), é melhor procurar ajuda médica.';
-        }
+  // Determinar a classificação do IMC usando o operador switch
+  let classificacao;
+  switch (true) {
+    case valorIMC <= 18.5:
+      classificacao = 'abaixo do peso';
+      break;
+    case valorIMC <= 24.9:
+      classificacao = 'no peso ideal. Parábens!!!';
+      break;
+    case valorIMC <= 29.9:
+      classificacao = 'levemente acima do peso';
+      break;
+    case valorIMC <= 34.9:
+      classificacao = 'com obesidade grau I, é melhor procurar ajuda médica.';
+      break;
+    case valorIMC <= 39.9:
+      classificacao = 'com obesidade grau II (severa), é melhor procurar ajuda médica.';
+      break;
+    default:
+      classificacao = 'com obesidade grau III (mórbida), é melhor procurar ajuda médica.';
+  }
 
-        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
-
-    }else{
-        resultado.textContent = 'Preencha todos os campos!!!'
-    }
-
+  // Exibir o resultado para o usuário
+  resultado.textContent = `${nome}, seu IMC é ${valorIMC} e você está ${classificacao}`;
 }
 
-calcular.addEventListener('click', imc);
+// Adicionar um evento de clique ao botão calcular
+calcular.addEventListener('click', calcularIMC);
